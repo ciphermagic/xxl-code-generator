4c7f0bfb57a0f963e447800bc9ef31ca561fb8b2
ciphermagic <ciphermagic@yeah.net>
2018-07-16 12:57:49 +0800

    bug fix & modify model.ftl

--- xxl-code-generator-admin/src/main/java/com/xxl/codegenerator/admin/core/util/TableParseUtil.java
+++ xxl-code-generator-admin/src/main/java/com/xxl/codegenerator/admin/core/util/TableParseUtil.java
@@ -7,6 +7,7 @@ import com.xxl.codegenerator.admin.core.model.FieldInfo;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -25,23 +26,23 @@ public class TableParseUtil {
      * @return
      */
     public static ClassInfo processTableIntoClassInfo(String tableSql) throws IOException {
-        if (tableSql==null || tableSql.trim().length()==0) {
+        if (tableSql == null || tableSql.trim().length() == 0) {
             throw new CodeGenerateException("Table structure can not be empty.");
         }
-        tableSql = tableSql.trim();
+        tableSql = tableSql.trim().toLowerCase();
 
         // table Name
         String tableName = null;
         if (tableSql.contains("TABLE") && tableSql.contains("(")) {
-            tableName = tableSql.substring(tableSql.indexOf("TABLE")+5, tableSql.indexOf("("));
+            tableName = tableSql.substring(tableSql.indexOf("TABLE") + 5, tableSql.indexOf("("));
         } else if (tableSql.contains("table") && tableSql.contains("(")) {
-            tableName = tableSql.substring(tableSql.indexOf("table")+5, tableSql.indexOf("("));
+            tableName = tableSql.substring(tableSql.indexOf("table") + 5, tableSql.indexOf("("));
         } else {
             throw new CodeGenerateException("Table structure anomaly.");
         }
 
         if (tableName.contains("`")) {
-            tableName = tableName.substring(tableName.indexOf("`")+1, tableName.lastIndexOf("`"));
+            tableName = tableName.substring(tableName.indexOf("`") + 1, tableName.lastIndexOf("`"));
         }
 
         // class Name
@@ -52,12 +53,12 @@ public class TableParseUtil {
 
         // class Comment
         String classComment = null;
-        if (tableSql.contains("COMMENT=")) {
-            String classCommentTmp = tableSql.substring(tableSql.lastIndexOf("COMMENT=")+8).trim();
-            if (classCommentTmp.contains("'") || classCommentTmp.indexOf("'")!=classCommentTmp.lastIndexOf("'")) {
-                classCommentTmp = classCommentTmp.substring(classCommentTmp.indexOf("'")+1, classCommentTmp.lastIndexOf("'"));
+        if (tableSql.contains("comment=")) {
+            String classCommentTmp = tableSql.substring(tableSql.lastIndexOf("comment=") + 8).trim();
+            if (classCommentTmp.contains("'") || classCommentTmp.indexOf("'") != classCommentTmp.lastIndexOf("'")) {
+                classCommentTmp = classCommentTmp.substring(classCommentTmp.indexOf("'") + 1, classCommentTmp.lastIndexOf("'"));
             }
-            if (classCommentTmp!=null && classCommentTmp.trim().length()>0) {
+            if (classCommentTmp != null && classCommentTmp.trim().length() > 0) {
                 classComment = classCommentTmp;
             }
         }
@@ -65,14 +66,14 @@ public class TableParseUtil {
         // field List
         List<FieldInfo> fieldList = new ArrayList<FieldInfo>();
 
-        String fieldListTmp = tableSql.substring(tableSql.indexOf("(")+1, tableSql.lastIndexOf(")"));
+        String fieldListTmp = tableSql.substring(tableSql.indexOf("(") + 1, tableSql.lastIndexOf(")"));
 
         // replave "," by "，" in comment
-        Matcher matcher = Pattern.compile("\\ COMMENT '(.*?)\\'").matcher(fieldListTmp);     // "\\{(.*?)\\}"
-        while(matcher.find()){
+        Matcher matcher = Pattern.compile("\\ comment '(.*?)\\'").matcher(fieldListTmp);     // "\\{(.*?)\\}"
+        while (matcher.find()) {
 
             String commentTmp = matcher.group();
-            commentTmp = commentTmp.replaceAll("\\ COMMENT '|\\'", "");      // "\\{|\\}"
+            commentTmp = commentTmp.replaceAll("\\ comment '|\\'", "");      // "\\{|\\}"
 
             if (commentTmp.contains(",")) {
                 String commentTmpFinal = commentTmp.replaceAll(",", "，");
@@ -83,13 +84,13 @@ public class TableParseUtil {
 
         String[] fieldLineList = fieldListTmp.split(",");
         if (fieldLineList.length > 0) {
-            for (String columnLine :fieldLineList) {
-                columnLine = columnLine.trim();		                                        // `userid` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
-                if (columnLine.startsWith("`")){
+            for (String columnLine : fieldLineList) {
+                columnLine = columnLine.trim();                                                // `userid` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+                if (columnLine.startsWith("`")) {
 
                     // column Name
-                    columnLine = columnLine.substring(1);			                        // userid` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
-                    String columnName = columnLine.substring(0, columnLine.indexOf("`"));	// userid
+                    columnLine = columnLine.substring(1);                                    // userid` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+                    String columnName = columnLine.substring(0, columnLine.indexOf("`"));    // userid
 
                     // field Name
                     String fieldName = StringUtils.lowerCaseFirst(StringUtils.underlineToCamelCase(columnName));
@@ -98,18 +99,18 @@ public class TableParseUtil {
                     }
 
                     // field class
-                    columnLine = columnLine.substring(columnLine.indexOf("`")+1).trim();	// int(11) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+                    columnLine = columnLine.substring(columnLine.indexOf("`") + 1).trim();    // int(11) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
                     String fieldClass = Object.class.getSimpleName();
                     if (columnLine.startsWith("int") || columnLine.startsWith("tinyint") || columnLine.startsWith("smallint")) {
-                        fieldClass = Integer.TYPE.getSimpleName();
+                        fieldClass = Integer.class.getSimpleName();
                     } else if (columnLine.startsWith("bigint")) {
-                        fieldClass = Long.TYPE.getSimpleName();
+                        fieldClass = Long.class.getSimpleName();
                     } else if (columnLine.startsWith("float")) {
-                        fieldClass = Float.TYPE.getSimpleName();
+                        fieldClass = Float.class.getSimpleName();
                     } else if (columnLine.startsWith("double")) {
-                        fieldClass = Double.TYPE.getSimpleName();
+                        fieldClass = Double.class.getSimpleName();
                     } else if (columnLine.startsWith("datetime") || columnLine.startsWith("timestamp")) {
-                        fieldClass = Date.class.getSimpleName();
+                        fieldClass = Timestamp.class.getSimpleName();
                     } else if (columnLine.startsWith("varchar") || columnLine.startsWith("text")) {
                         fieldClass = String.class.getSimpleName();
                     } else if (columnLine.startsWith("decimal")) {
@@ -118,10 +119,10 @@ public class TableParseUtil {
 
                     // field comment
                     String fieldComment = null;
-                    if (columnLine.contains("COMMENT")) {
-                        String commentTmp = fieldComment = columnLine.substring(columnLine.indexOf("COMMENT")+7).trim();	// '用户ID',
-                        if (commentTmp.contains("'") || commentTmp.indexOf("'")!=commentTmp.lastIndexOf("'")) {
-                            commentTmp = commentTmp.substring(commentTmp.indexOf("'")+1, commentTmp.lastIndexOf("'"));
+                    if (columnLine.contains("comment")) {
+                        String commentTmp = fieldComment = columnLine.substring(columnLine.indexOf("comment") + 7).trim();    // '用户ID',
+                        if (commentTmp.contains("'") || commentTmp.indexOf("'") != commentTmp.lastIndexOf("'")) {
+                            commentTmp = commentTmp.substring(commentTmp.indexOf("'") + 1, commentTmp.lastIndexOf("'"));
                         }
                         fieldComment = commentTmp;
                     }
--- xxl-code-generator-admin/src/main/resources/templates/xxl-code-generator/model.ftl
+++ xxl-code-generator-admin/src/main/resources/templates/xxl-code-generator/model.ftl
@@ -1,33 +1,54 @@
+<#assign importDate = false />
+<#assign importId = false />
 <#if classInfo.fieldList?exists && classInfo.fieldList?size gt 0>
     <#list classInfo.fieldList as fieldItem >
-        <#if fieldItem.fieldClass == "Date">
-            <#assign importDdate = true />
+        <#if fieldItem.fieldClass == "Timestamp">
+            <#assign importDate = true />
+        </#if>
+        <#if fieldItem.columnName == "id">
+            <#assign importId = true />
         </#if>
     </#list>
 </#if>
-import java.io.Serializable;
-<#if importDdate>
-import java.util.Date;
+<#if importDate>
+import java.sql.Timestamp;
 </#if>
+<#if importId>
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+</#if>
+import com.tupperware.pos.domain.common.Base;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
 /**
 *  ${classInfo.classComment}
 *
-*  Created by xuxueli on '${.now?string('yyyy-MM-dd HH:mm:ss')}'.
+*  Created by cipher on '${.now?string('yyyy-MM-dd HH:mm:ss')}'.
 */
-public class ${classInfo.className} implements Serializable {
-    private static final long serialVersionUID = 42L;
+@Table(name = "${classInfo.tableName}")
+@ApiModel(value = "${classInfo.className}", description = "${classInfo.classComment}")
+public class ${classInfo.className} extends Base {
+
+    private static final long serialVersionUID = 1L;
 
 <#if classInfo.fieldList?exists && classInfo.fieldList?size gt 0>
 <#list classInfo.fieldList as fieldItem >
-    /**
-    * ${fieldItem.fieldComment}
-    */
+    <#if fieldItem.columnName == "id" >
+    @Id
+    @GeneratedValue(generator = "JDBC")
+    @ApiModelProperty("${fieldItem.fieldComment}")
+    private Long id;
+
+    </#if>
+    <#if fieldItem.columnName != "id" >
+    @ApiModelProperty(value = "${fieldItem.fieldComment}")
     private ${fieldItem.fieldClass} ${fieldItem.fieldName};
 
+    </#if>
 </#list>
 </#if>
-
 <#if classInfo.fieldList?exists && classInfo.fieldList?size gt 0>
 <#list classInfo.fieldList as fieldItem>
     public ${fieldItem.fieldClass} get${fieldItem.fieldName?cap_first}() {
